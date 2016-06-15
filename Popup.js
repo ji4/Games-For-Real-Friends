30243694ff4592a3df412d929af86e6e16053907
m9731526 <laugholouder@gmail.com>
2016-06-15 21:42:16 +0800

    Finish Basic function

--- Popup.js
+++ Popup.js
@@ -1,21 +1,56 @@
 var info;
+var currentID;
 
-function FillGameInfo(id) {
+function FillGameInfo(ids) {
+    ids = parseInt(ids);
+    console.log("Current: " + currentID + ", Passed in ID: " + ids);
+    
+    if(info[ids].id != ids){
+        alert("Ids don't match!");
+        reutrn;
+    }
+    currentID = ids;
+    console.log("Current: " + currentID + ", Passed in ID: " + ids);
+    console.log(info);
+     
+    document.getElementsByClassName("gameTitle")[0].innerHTML = info[ids].Title;
+    document.getElementsByClassName("gameDescription")[0].innerHTML = info[ids].Description;
+    document.getElementsByClassName("Genre")[0].innerHTML = info[ids].Genre;
+    document.getElementsByClassName("RDate")[0].innerHTML = info[ids].ReleaseDate;
+    document.getElementsByClassName("Lang")[0].innerHTML = info[ids].Language;
+    document.getElementsByClassName("gamePreview")[0].setAttribute("src", info[ids].ImageURL);
+    
 
 }
 
 function popup(event){
-    document.getElementsByClassName("Base")[0].width = "0";    
-    document.getElementsByClassName("Base")[0].style.display = "block";
-    document.getElementsByClassName("Base")[0].width = "68vw";
-    document.getElementsByClassName("Wrapper_Info")[0].opacity = 1;
-    document.getElementsByClassName("blackMask")[0].opacity = 0.4;
+    popReset();
+    document.getElementsByClassName("Base")[0].style.visibility = "visible";
+    
+    FillGameInfo(event.target.getAttribute("data-game"));
+    document.getElementsByClassName("Base")[0].style.width = "68vw";
+    document.getElementsByClassName("Wrapper_Info")[0].style.opacity = 1;
+    document.getElementsByClassName("blackMask")[0].style.pointerEvents = "all";
+    document.getElementsByClassName("blackMask")[0].style.opacity = 0.4;
+}
+
+function popReset(){
+    document.getElementsByClassName("Wrapper_Info")[0].style.opacity = 0;
+    document.getElementsByClassName("blackMask")[0].style.opacity = 0;;
+    document.getElementsByClassName("blackMask")[0].style.pointerEvents = "none";
+    document.getElementsByClassName("Base")[0].style.width = 0;    
+}
+
+function popSwitch(event){
+    console.log("current: " + currentID);
+    if (event.target.className=="ChevronLeft") FillGameInfo((currentID - 1)>=0? (currentID - 1):currentID);
+    else if (event.target.className=="ChevronRight")FillGameInfo((currentID + 1)>info.length? currentID:(currentID+1));
 }
 
 function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'file.json', true);
+    xobj.open('GET', 'gameinfo.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
 
@@ -28,7 +63,9 @@ function loadJSON(callback) {
 
 }
 
-// Call to function with anonymous callback
-loadJSON(function (response) {
-    info = JSON.parse(response);
+function InitJSON(){// Call to function with anonymous callback
+    loadJSON(function (response) {
+        info = JSON.parse(response);
 });
+    
+}
